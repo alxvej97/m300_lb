69427ee7c6fc281146d6cda667b0f0054f729e5a
albin.vejseli@edu.tbz.ch <albin.vejseli@edu.tbz.ch>
2021-03-25 21:02:26 +0100

    PHP installiert

---
+++ lb2/MeineVagrantVM/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="de">
+
+<head>
+
+        <meta charset="UTF-8">
+
+        <title>PHP Website M300</title>
+
+        <link href="style.css" rel="stylesheet">
+
+</head>
+
+
+
+<body>
+
+Hallo das ist der Webserver vom M300
+
+</body>
+
+</html>
