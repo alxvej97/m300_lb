dc22a0a92f8b1b7e9ba1295b3b7189e14a4658b9
albin.vejseli@edu.tbz.ch <albin.vejseli@edu.tbz.ch>
2021-05-02 20:16:34 +0200

    Verbindung mit indexfile und MySQL Datenbank funktioniert

--- lb3/src/index.php
+++ lb3/src/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "hello from Docker";
+echo "Sie befinden sich nun auf meinem Docker lb3 container";
+
+$mysqli = new mysqli("db", "root", "example", "LB3");
+
+$sql = "INSERT INTO Checkliste (Handlung, Status) VALUES('PHP-Container', 'Erledigt')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO Checkliste (Handlung, Status) VALUES('Adminer-Container', 'Erledigt')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO Checkliste (Handlung, Status) VALUES('MySQL-Container', 'Erledigt')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO Checkliste (Handlung, Status) VALUES('Dokumentation', 'Erledigt')";
+$result = $mysqli->query($sql);
+
+
+$sql = 'SELECT * FROM Checkliste';
+
+if ($result = $mysqli->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->Handlung . " " . $user->Status;
+    echo "<br>";
+}
